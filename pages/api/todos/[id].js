b577d2912ad75fb8c2c2e8000bcd1461a2185eb9
Abdullah Younus <abdullah1001k@gmail.com>
2023-01-23 02:05:26 +0500

    local changes

--- pages/api/todos/[id].js
+++ pages/api/todos/[id].js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     if (req.method === "DELETE") {
         await prisma.todo.delete({
             where: {
-                id: parseInt(id)
+                id: +id
             }
         });
         res.status(200).json({ message: "Todo deleted" });
