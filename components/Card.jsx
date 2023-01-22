b654d65c98fdc2bb764b23a922804e50c212e724
Abdullah Younus <abdullah1001k@gmail.com>
2023-01-23 01:52:07 +0500

    change

--- components/Card.jsx
+++ components/Card.jsx
@@ -20,10 +20,6 @@ const Card = ({ tododata }) => {
                 <h1 className="text-2xl">{tlist.todo}</h1>
                 <p>{tlist.date}</p>
             </div>
-            <div className="mt-4 text-right">
-                <Link className='bg-black border rounded-md hover:bg-amber-700 hover:text-slate-200 px-6 py-2 text-bold text-green-400'
-                    href={`/todolist/${tlist.id}`}>Detail</Link>
-            </div>
         </div>
     ))
 }
